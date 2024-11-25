153b0c82d19d50a6362b3cbdf01631f2329b3311
Mathias Rossignol <mathias@fpt.com.vn>
2024-11-25 14:51:34 +0700

    Sync

--- Sound/WaveGuide/waveguide.js
+++ Sound/WaveGuide/waveguide.js
@@ -11,13 +11,15 @@
     const cicadaGain = wa.gain({gain:0.5}, wa.destination);
     envelopeFollower.connect(cicadaGain);
     const buffer = await wa.loadBuffer('cicadas-2.wav');
-    const instrument = await wa.loadBuffer('guitar-3.wav');
+    const instrument = await wa.loadBuffer('guitar-4.wav');
     const cicadaSource = wa.bufferSource('cicadas', {buffer: buffer, loop: true}, envelopeFollower);
     const guitarSource = wa.bufferSource('guitar', {buffer: instrument, loop: true}, 
-                                         wa.gain('gain', {gain: 0},
-                                                 wa.filter('filter', {type:'lowpass', frequency: 5000, Q:4}, wa.destination)
-                                                )
-				        );
+                                         wa.filter({type:'highshelf', frequency: 4000, gain: 8},
+						   wa.gain('gain', {gain: 0},
+							   wa.filter('filter', {type:'lowpass', frequency: 5000, Q:4}, wa.destination)
+							  )
+						  )
+					);
     envelopeFollower.port.onmessage = (data) => {
       wa.nodes.filter.frequency.value = 40000*data.data.value*data.data.value*data.data.value;
       const fadeIn = wa.ac.currentTime < 20 ? (wa.ac.currentTime*wa.ac.currentTime/400) : 1;
